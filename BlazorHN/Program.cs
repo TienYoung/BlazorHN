5af651c002fe1fc90917d7dde807215baa9ef281
Tien Young <tien.young@outlook.com>
2026-01-03 15:21:39 -0500

    Integrate Hacker News API with blazorwasm

--- BlazorHN/Program.cs
+++ BlazorHN/Program.cs
@@ -1,4 +1,5 @@
 using BlazorHN;
+using BlazorHN.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HackerNewsService(new HttpClient { BaseAddress = new Uri("https://hacker-news.firebaseio.com/v0/") }));
 
 await builder.Build().RunAsync();
---
+++ BlazorHN/Services/HackerNewsService.cs
@@ -0,0 +1,20 @@
+﻿using BlazorHN.Models;
+using System.Net.Http.Json;
+
+namespace BlazorHN.Services
+{
+    public class HackerNewsService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HackerNewsService(HttpClient httpClient)
+        { 
+            _httpClient = httpClient;
+        }
+
+        public async Task<HackerNewsItem?> GetItemAsync(int id)
+        { 
+            return await _httpClient.GetFromJsonAsync<HackerNewsItem>($"item/{id}.json");
+        }
+    }
+}
